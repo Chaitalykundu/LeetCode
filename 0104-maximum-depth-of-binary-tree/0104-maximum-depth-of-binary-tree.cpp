857dc4c74f508d0ad7618261eff19c0df4edaf82
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-02-16 11:27:42 +0530

    Time: 12 ms (48.69%), Space: 18.8 MB (92.80%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -14,6 +14,12 @@ public:
     int maxDepth(TreeNode* root) {
         if(root==NULL)
             return 0;
-        return max(maxDepth(root->right),maxDepth(root->left))+1;
+    
+        int depthOfLeft = maxDepth(root->left);
+        int depthOfRight = maxDepth(root->right);
+        
+        if(depthOfLeft>depthOfRight)
+            return depthOfLeft + 1;
+        return depthOfRight + 1;
     }
 };
