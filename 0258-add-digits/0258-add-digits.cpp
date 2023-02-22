757801110405d5757fce2f8e567662de86891d80
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-02-22 12:58:07 +0530

    Time: 3 ms (56.30%), Space: 6 MB (10.20%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
-    
-    int sumOfDigits(int n){
-        
-        int d, res=0;
-        while(n>0){
-            d = n%10;
-            res+=d;
-            n/=10;
-        }
-        return res;
-    }
-    
     int addDigits(int num) {
-        
-        int ans =0 ;
         if(num<=9)
             return num;
-        else if(num>9)
-            ans = sumOfDigits(num);
-        
-        return addDigits(ans);
+        else if(num%9==0)
+            return 9;
+        return num%9;
     }
 };
