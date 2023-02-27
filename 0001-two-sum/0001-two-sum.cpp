546691935dff0c0cacdcd93e3def42cd697738ff
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-02-27 16:19:54 +0530

    Time: 413 ms (33.27%), Space: 10.2 MB (73.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,19 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         int n = nums.size();
-        
+        int c=0;
         vector<int> ans;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
                     ans.push_back(i);
                     ans.push_back(j);
+                    c++;
+                    break;
                 }
             }
+            if(c!=0)
+                break;
         }
         return ans;
     }
