b34c7407b487337a8b59b4067a2f344617d4607c
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-03-23 10:08:47 +0530

    Time: 43 ms (43.85%), Space: 27.6 MB (18.45%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int len = nums.size();
+        
+        int l=0, h=len-1, mid;
+        
+        while(l<=h){
+            mid = (l+h)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                l=mid+1;
+            else
+                h = mid-1;
+        }
+        return -1;
+    }
+};
