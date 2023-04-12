6c3935d97f09d09d17cef7b421e4a2486c9f1765
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-04-12 12:05:59 +0530

    Time: 24 ms (52.55%), Space: 23.3 MB (50.76%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=0,h=s.size()-1;
+        
+        while(l<=h){
+            int temp = s[l];
+            s[l] = s[h];
+            s[h] = temp;
+            l++;
+            h--;
+        }
+    }
+};
