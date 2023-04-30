d35999bdad787c898b9cff85a69b3f04536858e1
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-04-30 14:35:28 +0530

    Time: 11 ms (19.15%), Space: 9.3 MB (7.55%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,50 +1,31 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
-public:
-    Node* cloneGraph(Node* node) {
-        if (node == nullptr) {
-            return nullptr;
-        }
-        
-        unordered_map<Node*, Node*> visited;
-        visited[node] = new Node(node->val);
-        
-        queue<Node*> que;
-        que.push(node);
-        
-        while (!que.empty()) {
-            Node* curr = que.front();
-            que.pop();
-            
-            for (auto neighbor : curr->neighbors) {
-                if (!visited.count(neighbor)) {
-                    visited[neighbor] = new Node(neighbor->val);
-                    que.push(neighbor);
+    public:
+    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
+    {
+        vector<Node*> neighbour;
+        Node* clone=new Node(cur->val);
+        mp[cur]=clone;
+            for(auto it:cur->neighbors)
+            {
+                if(mp.find(it)!=mp.end())   //already clone and stored in map
+                {
+                    neighbour.push_back(mp[it]);    //directly push back the clone node from map to neigh
                 }
-                visited[curr]->neighbors.push_back(visited[neighbor]);
+                else
+                    neighbour.push_back(dfs(it,mp));
             }
+            clone->neighbors=neighbour;
+            return clone;
+    }
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*,Node*> mp;
+        if(node==NULL)
+            return NULL;
+        if(node->neighbors.size()==0)   //if only one node present no neighbors
+        {
+            Node* clone= new Node(node->val);
+            return clone; 
         }
-        
-        return visited[node];  
+        return dfs(node,mp);
     }
 };
