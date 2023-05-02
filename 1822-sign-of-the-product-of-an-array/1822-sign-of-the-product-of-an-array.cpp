8a4987e8c5d212e5fcb93ce40880faeae5c38adb
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-05-02 11:23:58 +0530

    Time: 8 ms (27.40%), Space: 10.1 MB (99.27%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -3,25 +3,17 @@ public:
     int arraySign(vector<int>& nums) {
         
         int n = nums.size();
+
+        int ans=1;
         
-        long long int product = 1;
-        
-        for(int i=0;i<n;i++){
-            product*=nums[i];
-            
-            if(product==0)
-            return 0;
-            
-            else if(product>0)
-                product = 1;
+        for(auto i : nums){
+            if(i>0)
+                ans *=1;
+            else if(i<0)
+                ans *=-1;
             else
-                product = -1;
+                return 0;
         }
-        
-        
-        if(product>0)
-            return 1;
-        else
-            return -1;
+        return ans;
     }
 };
