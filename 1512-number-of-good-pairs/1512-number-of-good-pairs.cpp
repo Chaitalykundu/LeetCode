8f979394b07e4968e464fefcf88ab60c06410840
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-10-03 11:08:31 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (7.10%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int numIdenticalPairs(vector<int>& nums) {
-        int n = nums.size();
-        
-        vector<int> temp(1000);
-        
-        int c=0;
-        
-        // here num means the number of every index one by one
-        
-        for(int num:nums)
-            c+= temp[num]++;
-        
-        // alternative
-        
-//         for(int i=0;i<n;i++){
-//             c+=temp[nums[i]]++;
-//         }
-        
-        return c;
+    int numIdenticalPairs(std::vector<int>& nums) {
+        std::unordered_map<int, int> count;
+        int result = 0;
+
+        for (int num : nums) {
+            if (count.find(num) != count.end()) {
+                result += count[num];
+                count[num]++;
+            } else {
+                count[num] = 1;
+            }
+        }
+
+        return result;
     }
 };
-
-
-/*
- T.C = O(n)
-*/
