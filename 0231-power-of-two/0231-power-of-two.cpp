10522f1534018b39766bf3e4ed8b6188c3d958cf
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-02-19 21:05:08 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (39.77%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        if (n == 1)
+      return true;
+     
+    // all other odd numbers are not powers of 2
+    else if (n % 2 != 0 || n ==0)
+      return false;
+     
+    // recursive function call
+    return isPowerOfTwo(n / 2);
+    }
+};
