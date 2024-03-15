52159260386e1e7743ffa6268145bbf4d679ee61
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-03-15 13:56:42 +0530

    Time: 144 ms (14.45%), Space: 12.5 MB (84.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        int n = nums.size();
-        int c=0;
-        vector<int> ans;
+        int n= nums.size();
+        vector<int> v;
+        
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    c++;
+                if((nums[i]+nums[j])==target)
+                {
+                    v.push_back(i);
+                    v.push_back(j);
                     break;
                 }
             }
-            if(c!=0)
-                break;
         }
-        return ans;
+        return v;
     }
 };
