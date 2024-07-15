3b979c4f81acfa8a0f37cde35d4fdd932c84b086
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-07-15 11:47:31 +0530

    Time: 803 ms (25.33%), Space: 298.8 MB (24.89%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> m;
+        unordered_set<int> children;
+        TreeNode* root = nullptr;
+        for (auto i : descriptions)
+        {
+            if (!m[i[0]])
+            {
+                m[i[0]] = new TreeNode{i[0], nullptr, nullptr};
+            }
+            if (!m[i[1]])
+            {
+                m[i[1]] = new TreeNode{i[1], nullptr, nullptr};
+            }
+            if (i[2] == 1)
+            {
+                m[i[0]]->left = m[i[1]];
+            }
+            else {
+                m[i[0]]->right = m[i[1]];
+            }
+            children.insert(i[1]);
+        }
+        for (auto i : descriptions)
+        {
+            if (children.find(i[0]) == children.end())
+            {
+                root = m[i[0]];
+                cout << root;
+                return root;
+            }
+        }
+        return root;
+    }
+};
