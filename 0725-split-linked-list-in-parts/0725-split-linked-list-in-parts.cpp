19e92c4c2431963e76927f4df6a52919b94e5cb4
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-09-08 11:53:12 +0530

    Time: 10 ms (6.71%), Space: 13.8 MB (61.52%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
+   int countNodes(ListNode* head) {
+        int n = 0;
+        while (head) {
+            n++;
+            head = head->next;
+        }
+        return n;
+    }
+    
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        int length = 0;
-        ListNode* current = head;
-        vector<ListNode*> parts;
-
-        while (current) {
-            length++;
-            current = current->next;
-        }
-
-        int base_size = length / k, extra = length % k;
-        current = head;
-
-        for (int i = 0; i < k; ++i) {
-            int part_size = base_size + (extra > 0);
-            ListNode* part_head = nullptr, * part_tail = nullptr;
-
-            for (int j = 0; j < part_size; ++j) {
-                if (!part_head) {
-                    part_head = part_tail = current;
-                } else {
-                    part_tail->next = current;
-                    part_tail = part_tail->next;
-                }
-
-                if (current) {
-                    current = current->next;
-                }
+        int n = countNodes(head);
+        int part_size = n / k, left = n % k;
+        ListNode *ptr = head, *prev = NULL;
+        
+        vector<ListNode*> res(k);
+        for (int i = 0; i < k; i++) {
+            res[i] = head;
+            
+            for (int j = 0; j < part_size + (i < left); j++) {
+                prev = head; 
+                head = head->next; 
             }
-
-            if (part_tail) {
-                part_tail->next = nullptr;
-            }
-
-            parts.push_back(part_head);
-            extra = max(extra - 1, 0);
+            if (prev) prev->next = NULL;
         }
-
-        return parts;
+        
+        return res;
     }
 };
