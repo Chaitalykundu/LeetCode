4c5fe023bbb49a75b9fe810870c0650056de622a
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-09-23 10:35:07 +0530

    Time: 144 ms (46.62%), Space: 86.7 MB (57.94%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    map<string,int>m;
-    int c(int i,string &s,vector<int>&dp){
-        if(i>=s.size())return 0;
-        int ans=INT_MAX;
-        if(dp[i]!=-1)return dp[i];
-        ans = 1 + c(i+1,s,dp);
-        string k="";
-        for(int j=i;j<s.size();j++){
-            k+= s[j];
-            if(m[k]){
-                ans=min(ans,c(j+1,s,dp));
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_set<string> dict;
+        vector<int> dp(s.size() + 1, 0); // each cell represents the maximum number of 0 ~ i matched charactors.
+        for(auto& str: dictionary) {
+            dict.insert(str);
+        }
+        for(int l = 0; l < s.size(); ++l) {
+            for(int r = l; r < s.size(); ++r) {
+                int len = r - l + 1;
+                if(dict.count(s.substr(l, len))) {
+                    dp[r + 1] = max(dp[r + 1], dp[l] + len);
+                } else {
+                    dp[r + 1] = max(dp[r + 1], dp[r]);
+                }
             }
         }
-        return dp[i]=ans;
-    }
-    int minExtraChar(string s, vector<string>& d) {
-        for(auto i : d)m[i]++;
-        vector<int>dp(s.size(),-1);
-        return c(0,s,dp);
+        return s.size() - dp[s.size()];
     }
 };
